d04d1244032deff2848851f39b98d81c0a0d51ee
Pablo Martínez <pablomartinez1017@gmail.com>
2020-10-28 18:09:34 +0100

    YAML correcto con Bunny

---
+++ src/main/java/es/uva/inf/grammar/Pruebas.java
@@ -0,0 +1,12 @@
+package es.uva.inf.grammar;
+
+public class Pruebas {
+   public static void main (String args[]){
+       String test="********************************************************"+
+"	.Control"+
+"********************************************************~"+
+"		Simulation Control Parameters"+
+"	|";
+        System.out.println(test.length());
+   } 
+}
--- src/main/java/es/uva/inf/grammar/parser/visitors/EvalVisitor.java
+++ src/main/java/es/uva/inf/grammar/parser/visitors/EvalVisitor.java
@@ -33,43 +33,73 @@ public class EvalVisitor extends ModelBaseVisitor<String> {
     public String visitFile(ModelParser.FileContext ctx){
         try{
             int lastLine = countFileLines(input);
+            int lastLineLength = lastLineLength(input, lastLine) + 2;
             
             int equationsEndLine=0;
             int equationsHeader=0;
-            int equationsFooter=0;
+            int equationsFooter=9;
 
             String text = new String(Files.readAllBytes(Paths.get(input)),StandardCharsets.UTF_8);
+
+            String viewsDelimiter = "aaa---///";
+            viewsDelimiter = viewsDelimiter.replaceAll("a", "\\\\");
+            String[] graphs = text.split(viewsDelimiter,2);
             
-            String[] noControl = text.split("\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*\\*",2); 
-            equationsEndLine = countFileLines(noControl[0]);
+            equationsEndLine = graphs[0].split("\n").length;
 
             List<ModelParser.SymbolWithDocContext> equations = ctx.model().symbolWithDoc();
             for(int i=0;i<equations.size();i++){
                 int a = equations.get(i).start.getStartIndex();
                 int b = equations.get(i).stop.getStopIndex();
                 Interval interval = new Interval(a,b);
-                System.out.println(ctx.start.getInputStream().getText(interval));
-                System.out.println("DIFF");
+                String equation = ctx.start.getInputStream().getText(interval);
+                equationsFooter+=equation.length();
+                equationsFooter+=2; //Posiblemente la última línea (sin contar la línea de \r\n no cuente el salto de línea)
+                equationsFooter+=2; // \r + \n
             }
-            
+            equationsFooter+=166; //Control**** characters APROX, MUST BE CHANGED
+
 
             File outF = new File(output);
             FileWriter fw = new FileWriter(outF);
-            /*
+            
             fw.write("---\n");
             fw.write("type: file\n");
             fw.write("name: "+input+"\n");
-            fw.write("locationSpan : {start: [1,0], end: [" + lastLine + ",1]}\n");
+            fw.write("locationSpan : {start: [1,0], end: [" + (lastLine) + ","+lastLineLength+"]}\n");
             fw.write("footerSpan : [0,-1]\n");
             fw.write("parsingErrorsDetected : false\n");
             fw.write("children:\n");
             fw.write("  - type : equations\n");
-            fw.write("    locationSpan : {start: [1,0], end: [" + equationsEndLine + ",1]}\n");
+            fw.write("    locationSpan : {start: [1,0], end: [" + (equationsEndLine-1) + ",1]}\n");
             fw.write("    headerSpan : [0, 8]\n"); //Assuming file will always start with {UTF-8}, WILL BE CHANGED
             fw.write("    footerSpan : [" + equationsFooter + ", " + equationsFooter + "]\n");
             fw.write("    children :\n");
-            TEST
-            */
+            int locationSpanStartEq = 2; //Assuming there is always an encoding line {UTF-8}
+            int initCharEq = 9; //Assuming there is always an encoding line {UTF-8}
+            for(int i =0; i<equations.size();i++){
+                fw.write("    - type : equation\n");
+                fw.write("      name : "+equations.get(i).symbolWithDocDefinition().equation().lhs().Id().getText()+"\n");
+                int a = equations.get(i).start.getStartIndex();
+                int b = equations.get(i).stop.getStopIndex();
+                Interval interval = new Interval(a,b);
+                String equation = ctx.start.getInputStream().getText(interval);
+                if(equation.indexOf("FINAL TIME")!=-1){ //Control is reached
+                    locationSpanStartEq+=6;
+                    initCharEq+=166;
+                }
+                int equationNewLines = equation.split("\n").length; //One line for UTF-8 and other for \r\n
+                //System.out.println(equationNewLines);
+                fw.write("      locationSpan : {start: ["+locationSpanStartEq+",0], end: [" + (locationSpanStartEq+equationNewLines) + "," + "2" + "]}\n"); //It will always end in '\r \n'
+                locationSpanStartEq=locationSpanStartEq+equationNewLines+1;
+                int endCharEq = equation.length()+3; //\r \n + last \n that is not read by equation
+                fw.write("      span : [" + initCharEq + ", " + (endCharEq+initCharEq) + "]\n");
+                initCharEq=initCharEq+endCharEq +1; //+1 to get into next line
+            }
+            fw.write("  - type : sketches\n");
+            fw.write("    locationSpan : {start: ["+locationSpanStartEq+",0], end: [" + lastLine + ","+lastLineLength+"]}\n");
+            fw.write("    span : [" + (initCharEq+1) + ", " + (initCharEq+graphs[1].length()+9) + "]\n"); // 9 characters representing \\\---///, which was deleted in split()
+            
 
             fw.close();
         }catch(IOException ex){
@@ -82,13 +112,28 @@ public class EvalVisitor extends ModelBaseVisitor<String> {
         int lines=0;
         try{
             BufferedReader reader = new BufferedReader(new FileReader(input));
-            lines = 0;
             while (reader.readLine() != null) lines++;
             reader.close();
         }catch(IOException e){
             e.printStackTrace();
         }finally{
             return lines;
+        }
+    }
+
+    private int lastLineLength(String input, int lastLine){
+        int len=0;
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(input));
+            for(int i=0;i<lastLine-1;i++){
+                reader.readLine();
+            }
+            len = reader.readLine().length();
+            reader.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally{
+            return len;
         }
     }
 
