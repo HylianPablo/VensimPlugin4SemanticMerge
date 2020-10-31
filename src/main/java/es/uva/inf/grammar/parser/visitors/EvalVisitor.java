86dfed76634a50b28141f69cf30b6b0c33639fb2
Pablo Martínez <pablomartinez1017@gmail.com>
2020-10-31 13:36:38 +0100

    Added documentation and structure

--- src/main/java/es/uva/inf/grammar/parser/visitors/EvalVisitor.java
+++ src/main/java/es/uva/inf/grammar/parser/visitors/EvalVisitor.java
@@ -39,7 +39,6 @@ public class EvalVisitor extends ModelBaseVisitor<String> {
             int lastLineLength = lastLineLength(input, lastLine) + 2;
             
             int equationsEndLine=0;
-            //int equationsHeader=0;
             int equationsFooter=9;
 
             String text = new String(Files.readAllBytes(Paths.get(input)),StandardCharsets.UTF_8);
@@ -48,36 +47,13 @@ public class EvalVisitor extends ModelBaseVisitor<String> {
             viewsDelimiter = viewsDelimiter.replaceAll("a", "\\\\");
             String[] graphs = text.split(viewsDelimiter,2);
 
-            //String[] noControl = graphs[0].split("\t.Control",2); 
-            //String[] equationsLines = noControl[0].split("\\|");
             ArrayList<Boolean> newLinesInEquation = checkForNewLines(input);
-            /*
-            for(int i=0;i<equationsLines.length;i++){
-                newLinesInEquation.add(equationsLines[i].split("\n").length);
-            }
-            */
-            /*
-            for(int i=0;i<newLinesInEquation.size();i++){
-                System.out.println(newLinesInEquation.get(i));
-            }
-            */
             
             equationsEndLine = graphs[0].split("\n").length;
 
-
             List<ModelParser.SymbolWithDocContext> equations = ctx.model().symbolWithDoc();
-            /*
-            for(int i=0;i<equations.size();i++){
-                int a = equations.get(i).start.getStartIndex();
-                int b = equations.get(i).stop.getStopIndex();
-                Interval interval = new Interval(a,b);
-                String equation = ctx.start.getInputStream().getText(interval);
-                equationsFooter+=equation.length();
-                equationsFooter+=2; //Posiblemente la última línea (sin contar la línea de \r\n no cuente el salto de línea)
-                equationsFooter+=2; // \r + \n
-            }
-            */
-            equationsFooter+=166; //Control**** characters APROX, MUST BE CHANGED
+            
+            equationsFooter+=166; //Control delimiter characters APROX, MUST BE CHANGED
 
             equationsFooter=graphs[0].length()-3;
 
@@ -102,7 +78,7 @@ public class EvalVisitor extends ModelBaseVisitor<String> {
             reader.readLine();//UTF-8
             for(int i =0; i<equations.size();i++){
                 fw.write("    - type : equation\n");
-                String equationText;
+                String equationText;  //Mostly all strcutures are equations, but it will be cases that there will be subscriptRanges or others
                 if(equations.get(i).symbolWithDocDefinition().equation()!=null){
                     equationText=equations.get(i).symbolWithDocDefinition().equation().lhs().Id().getText();
                 }else{
@@ -112,43 +88,35 @@ public class EvalVisitor extends ModelBaseVisitor<String> {
                 int a = equations.get(i).start.getStartIndex();
                 int b = equations.get(i).stop.getStopIndex();
                 Interval interval = new Interval(a,b);
-                String equation = ctx.start.getInputStream().getText(interval);
-                //System.out.println(equation);
-                //System.out.println(equation.split("\n").length);
+                String equation = ctx.start.getInputStream().getText(interval); //Obtaining all text of equation without trimming
                 for(int j=0;j<equation.split("\n").length;j++){
                     reader.readLine();
                 }
                 Set<String> equationFollowingLines = new HashSet<String>();
-                reader.mark(400);
+                reader.mark(400);  //Arbitrary limit, an equation its supposed to have less than 400 characters, but it can be changed
                 for(int j =0;j<3;j++){
-                    equationFollowingLines.add(reader.readLine());
+                    equationFollowingLines.add(reader.readLine());  //Three lines are read due that it can exists various newlines between equations
                 }
                 reader.reset();
-                //System.out.println(actualLineRead);
-                //equationFollowingLines.forEach(System.out::println);
-                //System.out.println("NEXT");
                 if(equationFollowingLines.contains("\t.Control")){ //Control is reached MODIFICAR, NO ES NECESARIO LLEGAR A FINAL TIME
-                    System.out.println("LLEGA");
                     locationSpanStartEq+=6;
-                    initCharEq+=167;
+                    initCharEq+=167;    //Added in order to match Control Delimiter Characters
                 }
                 int endCharEq;
                 int endColumnLocationSpan = 2;
                 int equationNewLines = equation.split("\n").length; //One line for UTF-8 and other for \r\n
-                if(i<newLinesInEquation.size()){
+                if(i<newLinesInEquation.size()){ //It only checks until .Control delimiter
                     if(newLinesInEquation.get(i)){
                         endCharEq = equation.length()+3; //\r \n + last \n that is not read by equation
                         reader.readLine();
                     }else{
                         endCharEq = equation.length()+1;
-                        //locationSpanStartEq-=1;
                         endColumnLocationSpan = equation.split("\n")[equationNewLines-1].length() + 2; //\r \n
                         equationNewLines--;
                     }
                 }else{
                     endCharEq = equation.length()+3; //\r \n + last \n that is not read by equation
                 }
-                //System.out.println(equationNewLines);
                 fw.write("      locationSpan : {start: ["+locationSpanStartEq+",0], end: [" + (locationSpanStartEq+equationNewLines) + "," + endColumnLocationSpan + "]}\n"); //It will always end in '\r \n'
                 locationSpanStartEq=locationSpanStartEq+equationNewLines+1;
                 
