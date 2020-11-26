17d4be00b080574b986e611e6a564953eb995ece
Pablo Martínez <pablomartinez1017@gmail.com>
2020-11-26 15:45:35 +0100

    Eliminacion de delimitadores

---
+++ src/main/java/es/uva/inf/grammar/NoDelimiters.java
@@ -0,0 +1,30 @@
+package es.uva.inf.grammar;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class NoDelimiters {
+
+    public static void main(String args[]) {
+        try {
+            String text = new String(Files.readAllBytes(Paths.get(args[0])), StandardCharsets.UTF_8);
+            String[] newlines = text.split("\r\n");
+            //String outputName = args[0];
+            String outputName = "out.txt";
+            FileWriter writer = new FileWriter(outputName);
+            for (int i = 0; i < newlines.length; i++) {
+                if (!newlines[i].contains("<[VIEWS START]>") && !newlines[i].contains("<[VIEW START]>")
+                        && !newlines[i].contains("<[VIEW END]>") && !newlines[i].contains("<[VIEWS END]>")
+                        && !newlines[i].contains("<[GRAPH START]>") && !newlines[i].contains("<[GRAPH END]>")) {
+                    writer.write(newlines[i] + "\r\n");
+                }
+            }
+            writer.close();
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+}
